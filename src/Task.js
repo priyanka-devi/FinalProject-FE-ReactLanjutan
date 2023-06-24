1eb752e27edc7e61844f924cfaf590005c16389c
Priyanka Devi <priyanka.devi@ui.ac.id>
2023-06-24 18:50:09 +0700

    Fix deployment to firebase

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://finalproject-fe-reactlanjutan.web.app/" // TODO: replace this
 const studentName = "Priyanka Devi" // TODO: replace this
 const studentId = "FE4529989" // TODO: replace this
 
